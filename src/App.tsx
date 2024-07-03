513172279b855ab93aeda444c477994f06be795c
jokinz <53506251+jokinz@users.noreply.github.com>
2024-07-03 17:02:39 -0500

    moved navigation, context and theming logic to App comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,66 @@
 import './App.css'
 
+import { useState } from 'react'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import CssBaseline from '@mui/material/CssBaseline'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
+import { UserContext, UserContextType } from './contexts/UserContext.ts'
+
+import PageDashboard from './routes/PageDashboard.tsx'
+import PageEmployees from './routes/PageEmployees.tsx'
+import PageLogin from './routes/PageLogin.tsx'
+import PageMyAccount from './routes/PageMyAccount.tsx'
+import PageNotFound from './routes/PageNotFound.tsx'
+import PageReserves from './routes/PageReserves.tsx'
+import PageUsers from './routes/PageUsers.tsx'
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
+
 const App = () => {
-  return <h1>Paddle App</h1>
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <PageDashboard />,
+      errorElement: <PageNotFound />,
+    },
+    {
+      path: '/my-account',
+      element: <PageMyAccount />,
+    },
+    {
+      path: '/reserves',
+      element: <PageReserves />,
+    },
+    {
+      path: '/login',
+      element: <PageLogin />,
+    },
+    {
+      path: '/users',
+      element: <PageUsers />,
+    },
+    {
+      path: '/employees',
+      element: <PageEmployees />,
+    },
+  ])
+
+  const [user, setUser] = useState<UserContextType>(null)
+  return (
+    <UserContext.Provider value={user}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </UserContext.Provider>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,58 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-import CssBaseline from '@mui/material/CssBaseline'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
-
 import App from './App.tsx'
-import PageLogin from './routes/PageLogin.tsx'
-import PageMyAccount from './routes/PageMyAccount.tsx'
-import PageReserves from './routes/PageReserves.tsx'
-import PageUsers from './routes/PageUsers.tsx'
-import PageNotFound from './routes/PageNotFound.tsx'
-import PageEmployees from './routes/PageEmployees.tsx'
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-})
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <PageNotFound />,
-  },
-  {
-    path: '/my-account',
-    element: <PageMyAccount />,
-  },
-  {
-    path: '/reserves',
-    element: <PageReserves />,
-  },
-  {
-    path: '/login',
-    element: <PageLogin />,
-  },
-  {
-    path: '/users',
-    element: <PageUsers />,
-  },
-  {
-    path: '/employees',
-    element: <PageEmployees />,
-  },
-])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 )
