a0be8ff07082db8ef628667fa31e4108172d1074
jokinz <53506251+jokinz@users.noreply.github.com>
2024-07-06 23:24:05 -0500

    Create PageDisabled.tsx

---
+++ src/routes/PageDisabled.tsx
@@ -0,0 +1,12 @@
+import ButtonLogout from '../components/ButtonLogout'
+
+const PageDisabled = () => {
+  return (
+    <>
+      <h1>Su usuario está deshabilitado</h1>
+      <ButtonLogout />
+    </>
+  )
+}
+
+export default PageDisabled
