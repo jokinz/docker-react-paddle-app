5d20a219d94b06ca8013545624b8152180fbcd4d
jokinz <53506251+jokinz@users.noreply.github.com>
2024-07-06 23:22:42 -0500

    updated context structure

--- src/contexts/EmployeeContext.ts
+++ src/contexts/EmployeeContext.ts
@@ -2,6 +2,11 @@ import { createContext } from 'react'
 
 import { Employee } from '../types/employee'
 
-export type EmployeeContextType = Employee | null
+export type EmployeeContextType = {
+  employee: Employee | null
+  setEmployee: (newValue: Employee | null) => void
+}
 
-export const EmployeeContext = createContext<EmployeeContextType>(null)
+export const EmployeeContext = createContext<EmployeeContextType | undefined>(
+  undefined
+)
