abb0228c6ed48fd9a7546d8d7b5a0b588c18e117
jokinz <53506251+jokinz@users.noreply.github.com>
2024-07-07 17:11:32 -0500

    Update reserve.ts

--- src/api/reserves/reserve.ts
+++ src/api/reserves/reserve.ts
@@ -1,27 +1,49 @@
-export const createReserve = async () => {
-    return 
+import { Reserve as ReserveType } from '../../types/reserve'
+import { emulateApiCall, reserveExample, reservesListExample } from '../dummy'
+
+export const getReserveById = async (
+  id: number
+): Promise<ReserveType | undefined> => {
+  try {
+    const response: ReserveType = await emulateApiCall(
+      reserveExample,
+      'success',
+      id
+    )
+    if (response) {
+      return response
+    }
+    throw new Error('Error descargando datos de reserva')
+  } catch (error) {
+    console.error(error)
+  }
+  return
 }
 
-export const getReserveById = async (id: number) => {
-    try {
-        return
-    } catch (error) {
-        
-    } 
+export const updateReserveById = async (reserve: ReserveType) => {
+  try {
+    const response = await emulateApiCall(reserve, 'success')
+    if (response) {
+      return response
+    }
+    throw new Error('Error actualizando datos de reserva')
+  } catch (error) {
+    console.error(error)
+  }
+  return
 }
 
-export const getReservesByUserId = async (userId: number) => {
-    try {
-        return
-    } catch (error) {
-        
-    } 
-}
-
-export const updateReserveById = async (id: number) => {
-    try {
-        return
-    } catch (error) {
-        
-    } 
+export const getAllReserves = async (): Promise<ReserveType[] | undefined> => {
+  try {
+    const response: ReserveType[] = await emulateApiCall(
+      reservesListExample,
+      'success'
+    )
+    if (response) {
+      return response
+    }
+    throw new Error('Error descargando reservas')
+  } catch (error) {
+    console.error(error)
+  }
 }
