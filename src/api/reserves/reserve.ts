90dfc447eddf59da1ac0937db3ae2e3de541c545
jokinz <53506251+jokinz@users.noreply.github.com>
2024-07-06 20:44:21 -0500

    Create reserve.ts

---
+++ src/api/reserves/reserve.ts
@@ -0,0 +1,27 @@
+export const createReserve = async () => {
+    return 
+}
+
+export const getReserveById = async (id: number) => {
+    try {
+        return
+    } catch (error) {
+        
+    } 
+}
+
+export const getReservesByUserId = async (userId: number) => {
+    try {
+        return
+    } catch (error) {
+        
+    } 
+}
+
+export const updateReserveById = async (id: number) => {
+    try {
+        return
+    } catch (error) {
+        
+    } 
+}
