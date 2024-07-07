69e9954f5db52cf225f04fb4bcee5857753760df
jokinz <53506251+jokinz@users.noreply.github.com>
2024-07-07 17:12:29 -0500

    Update PageReserves.tsx

--- src/routes/PageReserves.tsx
+++ src/routes/PageReserves.tsx
@@ -1,5 +1,44 @@
+import { useEffect, useState } from 'react'
+
+import { Reserve } from '../types/reserve'
+
+import { getAllReserves } from '../api/reserves/reserve'
+import { reserveExample } from '../api/dummy'
+
+import ButtonLogout from '../components/ButtonLogout'
+import ForceLogin from '../components/ForceLogin'
+import LoadingWrapper from '../components/LoadingWrapper'
+import UpdateReserve from '../components/Reserves/UpdateReserve'
+import ReservesList from '../components/Reserves/ReservesList'
+
 const PageReserves = () => {
-  return <div>Página Reservas</div>
+  const [reserveList, setReserveList] = useState<Reserve[]>([])
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    const getData = async () => {
+      const result = await getAllReserves()
+      if (result) {
+        setReserveList(result)
+        setLoading(false)
+      } else {
+        setReserveList([])
+        setLoading(false)
+      }
+    }
+    getData()
+  }, [])
+  return (
+    <ForceLogin>
+      <div>
+        Página Reservas
+        <UpdateReserve reserve={reserveExample} />
+        <LoadingWrapper loading={loading}>
+          <ReservesList reserves={reserveList} />
+        </LoadingWrapper>
+      </div>
+      <ButtonLogout />
+    </ForceLogin>
+  )
 }
 
 export default PageReserves
