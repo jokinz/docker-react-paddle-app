6bb686d8c17d3f1701e660d53f59af9de3bea1c3
jokinz <53506251+jokinz@users.noreply.github.com>
2024-07-07 02:54:48 -0500

    Create UpdateItem.tsx

---
+++ src/components/Items/UpdateItem.tsx
@@ -0,0 +1,87 @@
+import { useState } from 'react'
+
+import { Item } from '../../types/item'
+
+import {
+    Button,
+    Grid,
+    Switch,
+    TextField
+} from '@mui/material'
+import { updateItemById, updateItemEnabledById } from '../../api/items/item'
+type props = {
+  item: Item
+}
+const UpdateItem = ({ item }: props) => {
+  const [itemData, setItemData] = useState<Item>(item)
+  const handleUpdateItemClick = async () => {
+    const response = await updateItemById(itemData)
+    console.log(response)
+  }
+  const handleEnabledClick = async () => {
+    setItemData((prev) => {
+      const updatedEnabled = !prev.enabled
+      return { ...prev, enabled: updatedEnabled }
+    })
+  }
+  return (
+    <Grid item xs={12}>
+      <Grid item xs={12}>
+        <h1>Actualizar item</h1>
+      </Grid>
+      <Grid item xs={6}>
+        <TextField
+          id="name"
+          label="Nombre"
+          variant="filled"
+          value={itemData.name}
+          onChange={(e) =>
+            setItemData((prev) => {
+              return { ...prev, name: e.target.value }
+            })
+          }
+        />
+      </Grid>
+      <Grid item xs={6}>
+        <TextField
+          id="description"
+          label="Descripción"
+          variant="filled"
+          value={itemData.description}
+          onChange={(e) =>
+            setItemData((prev) => {
+              return { ...prev, description: e.target.value }
+            })
+          }
+        />
+      </Grid>
+      <Grid item xs={6}>
+        <TextField
+          id="price"
+          label="Precio"
+          variant="filled"
+          value={itemData.price}
+          onChange={(e) =>
+            setItemData((prev) => {
+              return { ...prev, price: parseInt(e.target.value) }
+            })
+          }
+        />
+      </Grid>
+      <Grid item xs={6}>
+        <Switch
+          aria-label="Activado"
+          value={itemData.enabled}
+          onClick={handleEnabledClick}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Button variant="contained" onClick={handleUpdateItemClick}>
+          Actualizar item
+        </Button>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default UpdateItem
