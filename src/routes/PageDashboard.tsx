76c21c1dbddb69d8f5d67b985f1f3513cfd48932
jokinz <53506251+jokinz@users.noreply.github.com>
2024-07-06 23:32:26 -0500

    Update PageDashboard.tsx

--- src/routes/PageDashboard.tsx
+++ src/routes/PageDashboard.tsx
@@ -1,5 +1,31 @@
+import { Button } from '@mui/material'
+
 const PageDashboard = () => {
-  return <div>Página dashboard</div>
+  return (
+    <div>
+      Página dashboard
+      <div>
+        <Button variant="outlined" href="/login">
+          Login
+        </Button>
+      </div>
+      <div>
+        <Button variant="outlined" href="/reserves">
+          Reservas
+        </Button>
+      </div>
+      <div>
+        <Button variant="outlined" href="/users">
+          Usuarios
+        </Button>
+      </div>
+      <div>
+        <Button variant="outlined" href="/employees">
+          Empleados
+        </Button>
+      </div>
+    </div>
+  )
 }
 
 export default PageDashboard
