247ac5701aa2118f4d0ac4d6651b757c00943845
jokinz <53506251+jokinz@users.noreply.github.com>
2024-07-15 18:21:11 -0500

    updated ui

--- src/components/Payments/PaymentItem.tsx
+++ src/components/Payments/PaymentItem.tsx
@@ -1,55 +1,25 @@
 import {
-  FormControl,
-  InputLabel,
   ListItem,
-  ListItemText,
-  MenuItem,
-  Select,
-  SelectChangeEvent,
+  ListItemText
 } from '@mui/material'
 
-import { Payment as PaymentType, paymentStatus } from '../../types/payment'
+import { NavLink } from 'react-router-dom'
+import { Payment as PaymentType } from '../../types/payment'
 
 type props = {
   index: number
   payment: PaymentType
-  handlePaymentEnabledUpdate(index: number, payment: PaymentType): Promise<void>
 }
-const PaymentItem = ({ index, payment, handlePaymentEnabledUpdate }: props) => {
-  const handlePaymentStatusChange = (event: SelectChangeEvent) => {
-    const newPayment: PaymentType = {
-      ...payment,
-      status: event.target.value as paymentStatus,
-    }
-    handlePaymentEnabledUpdate(index, newPayment)
-  }
+const PaymentItem = ({ payment }: props) => {
   return (
     <ListItem
-      secondaryAction={
-        <FormControl variant="standard">
-          <InputLabel id="demo-simple-select-label">Estado de pago</InputLabel>
-          <Select
-            disabled={payment.status === 'paid' || payment.status === 'failed'}
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={payment.status}
-            label="Estado de pago"
-            onChange={handlePaymentStatusChange}
-          >
-            {/* TODO: consultar lógica para disabled */}
-            <MenuItem disabled value={'pending'}>
-              Pendiente
-            </MenuItem>
-            <MenuItem value={'paid'}>Pagado</MenuItem>
-            <MenuItem value={'failed'}>Fallido</MenuItem>
-          </Select>
-        </FormControl>
-      }
     >
-      <ListItemText
-        primary={`id: ${payment.id}`}
-        secondary={`idReserve: ${payment.idReserve} transactionCode: ${payment.transactionCode}`}
-      />
+      <NavLink to={`/payments/${payment.id}`}>
+        <ListItemText
+          primary={`id: ${payment.id}`}
+          secondary={`idReserve: ${payment.idReserve} transactionCode: ${payment.transactionCode}`}
+        />
+      </NavLink>
     </ListItem>
   )
 }
