fad93ad399a2cef32123c60bec948f262f601968
jokinz <53506251+jokinz@users.noreply.github.com>
2024-07-18 01:53:12 -0500

    Update ReserveItem.tsx

--- src/components/Reserves/ReserveItem.tsx
+++ src/components/Reserves/ReserveItem.tsx
@@ -1,44 +1,22 @@
-import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
 
-import { FormControlLabel, ListItem, ListItemText, Switch } from '@mui/material'
+import { ListItem, ListItemText } from '@mui/material'
 
 import { Reserve as ReserveType } from '../../types/reserve'
 
 type props = {
   index: number
   reserve: ReserveType
-  handleReserveEnabledUpdate(index: number, reserve: ReserveType): Promise<void>
 }
-const ReserveItem = ({ index, reserve, handleReserveEnabledUpdate }: props) => {
-  const [disabled, setDisabled] = useState(false)
-  const handleSwitchClick = async (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    e.preventDefault()
-    setDisabled(true)
-    await handleReserveEnabledUpdate(index, reserve)
-    setDisabled(false)
-  }
+const ReserveItem = ({ reserve }: props) => {
   return (
-    <ListItem
-      secondaryAction={
-        <FormControlLabel
-          control={
-            <Switch
-              disabled={disabled}
-              checked={reserve.itemsHanded}
-              onClick={(e) => handleSwitchClick(e)}
-            />
-          }
-          label="Items entregados:"
-          labelPlacement="start"
+    <ListItem>
+      <NavLink to={`/reserves/${reserve.id}`}>
+        <ListItemText
+          primary={`id: ${reserve.id}`}
+          secondary={`idReserve: ${reserve.id} Items entregados: ${reserve.itemsHanded}`}
         />
-      }
-    >
-      <ListItemText
-        primary={`id: ${reserve.id} idPlayingField: ${reserve.idPlayingField} idPlayingField: ${reserve.idStablishment} `}
-        secondary={`itemsHanded: ${reserve.itemsHanded}`}
-      />
+      </NavLink>{' '}
     </ListItem>
   )
 }
