84b7b640ec7beba6541614ff5d20341da19ea3e5
jokinz <53506251+jokinz@users.noreply.github.com>
2024-07-18 02:25:33 -0500

    updated ui

--- src/components/Users/UsersList.tsx
+++ src/components/Users/UsersList.tsx
@@ -1,27 +1,21 @@
 import { User as UserType } from '../../types/user'
 
-import { Grid, List, styled } from '@mui/material'
+import { Grid, List } from '@mui/material'
 
 import UserItem from './UserItem'
 
 type props = { users: UserType[] }
 const UsersList = ({ users }: props) => {
-  const Demo = styled('div')(({ theme }) => ({
-    backgroundColor: theme.palette.background.paper,
-  }))
-
   if (users.length > 0) {
     return (
       <div>
         Lista de usuarios
         <Grid item xs={12} md={6}>
-          <Demo>
-            <List dense={false}>
-              {users.map((user, index) => (
-                <UserItem key={index} user={user} />
-              ))}
-            </List>
-          </Demo>
+          <List dense={false}>
+            {users.map((user, index) => (
+              <UserItem key={index} user={user} />
+            ))}
+          </List>
         </Grid>
       </div>
     )
@@ -29,7 +23,7 @@ const UsersList = ({ users }: props) => {
 
   return (
     <div>
-      Lista de usuarios <br />
+      <h1>Lista de usuarios</h1>
       Ningún usuario para mostrar
     </div>
   )
