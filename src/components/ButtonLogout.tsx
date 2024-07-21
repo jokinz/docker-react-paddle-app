82c726be7bfb342efbdf8a0a6c4a61dede2f6f47
jokinz <53506251+jokinz@users.noreply.github.com>
2024-07-20 22:21:47 -0500

    updated ui

--- src/components/ButtonLogout.tsx
+++ src/components/ButtonLogout.tsx
@@ -1,18 +1,39 @@
-import { useContext } from 'react'
-
-import { EmployeeContext } from '../contexts/EmployeeContext'
+import { useContext, useState } from 'react'
 
 import { Button } from '@mui/material'
 
+import { enqueueSnackbar } from 'notistack'
+
+import { EmployeeContext } from '../contexts/EmployeeContext'
+
+import { emulateApiCall } from '../api/dummy'
+
+import LoadingWrapper from './LoadingWrapper'
+
 const ButtonLogout = () => {
   const employeeContext = useContext(EmployeeContext)
-  const handleLogoutClick = () => {
-    employeeContext?.setEmployee(null)
+  const [loading, setLoading] = useState(false)
+  const handleLogoutClick = async () => {
+    setLoading(true)
+    try {
+      const response = await emulateApiCall(
+        employeeContext?.employee,
+        'success'
+      )
+      if (response) {
+        employeeContext?.setEmployee(null)
+      }
+    } catch (error) {
+      enqueueSnackbar('Error al cerrar la sesión', { variant: 'error' })
+      console.error(error)
+    } finally {
+      setLoading(false)
+    }
   }
   return (
     <div>
       <Button onClick={handleLogoutClick} color="error" variant="outlined">
-        Cerrar sesión
+        <LoadingWrapper loading={loading}>Cerrar sesión</LoadingWrapper>
       </Button>
     </div>
   )
