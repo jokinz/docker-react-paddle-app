73c03af5cf47efd75879296318a24baee8c3b57c
jokinz <53506251+jokinz@users.noreply.github.com>
2024-07-24 13:09:50 -0500

    Update PaymentItem.tsx

--- src/components/Payments/PaymentItem.tsx
+++ src/components/Payments/PaymentItem.tsx
@@ -1,9 +1,7 @@
-import {
-  ListItem,
-  ListItemText
-} from '@mui/material'
+import { useNavigate } from 'react-router-dom'
 
-import { NavLink } from 'react-router-dom'
+import { Grid, ListItem, ListItemButton, ListItemText } from '@mui/material'
+
 import { Payment as PaymentType } from '../../types/payment'
 
 type props = {
@@ -11,15 +9,26 @@ type props = {
   payment: PaymentType
 }
 const PaymentItem = ({ payment }: props) => {
+  const navigate = useNavigate()
+  const handleClick = () => {
+    navigate(`/payments/${payment.id}`)
+  }
   return (
-    <ListItem
-    >
-      <NavLink to={`/payments/${payment.id}`}>
-        <ListItemText
-          primary={`id: ${payment.id}`}
-          secondary={`idReserve: ${payment.idReserve} transactionCode: ${payment.transactionCode}`}
-        />
-      </NavLink>
+    <ListItem>
+      <ListItemButton onClick={handleClick}>
+        <Grid container display={'flex'} alignItems={'center'}>
+          <ListItemText primary={`Número de pago: ${payment.id}`} />
+          <Grid item xs={3}>
+            Código de transacción: {payment.transactionCode}
+          </Grid>
+          <Grid item xs={3}>
+            Número de reserva: <b>{payment.idReserve}</b>
+          </Grid>
+          <Grid item xs={3}>
+            Estado del pago: <b>{`${payment.status}`}</b>
+          </Grid>
+        </Grid>
+      </ListItemButton>
     </ListItem>
   )
 }
