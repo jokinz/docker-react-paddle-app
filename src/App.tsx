0e0754445f8397007bb3d06602a6a1f4a5d51eb4
jokinz <53506251+jokinz@users.noreply.github.com>
2024-08-10 19:36:26 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css'
 
@@ -11,9 +11,9 @@ import { createTheme, ThemeProvider } from '@mui/material/styles'
 
 import { EmployeeContext } from './contexts/EmployeeContext.ts'
 
-import { employeeExample } from './api/dummy.ts'
-
 import { Employee } from './types/employee.ts'
+
+import { useCookies } from 'react-cookie'
 
 import PageDashboard from './routes/PageDashboard.tsx'
 import PageEmployees from './routes/PageEmployees.tsx'
@@ -23,12 +23,15 @@ import PageNotFound from './routes/PageNotFound.tsx'
 import PagePayments from './routes/PagePayments.tsx'
 import PagePaymentSingle from './routes/PagePaymentSingle.tsx'
 import PageReserves from './routes/PageReserves.tsx'
-import PageUsers from './routes/PageUsers.tsx'
 import PageReserveSingle from './routes/PageReserveSingle.tsx'
+import PageUsers from './routes/PageUsers.tsx'
+
+import LoadingWrapper from './components/LoadingWrapper.tsx'
+import { BOHEMIA_PADEL_JWT } from './types/userCookie.ts'
 
 const darkTheme = createTheme({
   palette: {
-    mode: 'light',
+    mode: 'dark',
   },
 })
 
@@ -73,20 +76,34 @@ const App = () => {
     },
   ])
 
-  const [employee, setEmployee] = useState<Employee | null>(employeeExample)
+  const [employee, setEmployee] = useState<Employee | null>(null)
+  const [loading, setLoading] = useState(true)
+
+  const [cookies] = useCookies([BOHEMIA_PADEL_JWT])
+  useEffect(() => {
+    const userCookie = cookies[BOHEMIA_PADEL_JWT]
+    if (userCookie) {
+      setEmployee(userCookie.data)
+    } else {
+      setEmployee(null)
+    }
+    setLoading(false)
+  }, [cookies])
   return (
-    <SnackbarProvider
-      maxSnack={3}
-      autoHideDuration={1500}
-      anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
-    >
-      <EmployeeContext.Provider value={{ employee, setEmployee }}>
-        <ThemeProvider theme={darkTheme}>
-          <CssBaseline />
-          <RouterProvider router={router} />
-        </ThemeProvider>
-      </EmployeeContext.Provider>
-    </SnackbarProvider>
+    <LoadingWrapper loading={loading}>
+      <SnackbarProvider
+        maxSnack={3}
+        autoHideDuration={1500}
+        anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+      >
+        <EmployeeContext.Provider value={{ employee, setEmployee }}>
+          <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <RouterProvider router={router} />
+          </ThemeProvider>
+        </EmployeeContext.Provider>
+      </SnackbarProvider>
+    </LoadingWrapper>
   )
 }
 
