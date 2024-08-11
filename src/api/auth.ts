5e0815130221c495dd4163b78b1f835e528977f6
jokinz <53506251+jokinz@users.noreply.github.com>
2024-08-10 19:28:25 -0500

    Create auth.ts

---
+++ src/api/auth.ts
@@ -0,0 +1,22 @@
+import { AuthResponse } from '../types/responses/AuthResponse'
+import axios from './axios'
+
+export const auth = async (
+  email: string,
+  password: string,
+  type: string = 'Employee'
+): Promise<AuthResponse | undefined> => {
+  try {
+    const axiosResponse = await axios.post('/auth', {
+      email,
+      password,
+      type,
+    })
+    if (axiosResponse.status && axiosResponse.data.statusCode === 200) {
+      return axiosResponse.data
+    }
+  } catch (error) {
+    console.error(error)
+    return
+  }
+}
