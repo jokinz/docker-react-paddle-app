358602b9fbd0d872d14a841939cff5ca06ca5b5b
jokinz <53506251+jokinz@users.noreply.github.com>
2024-08-15 13:27:01 -0500

    Update PageEmployees.tsx

--- src/routes/PageEmployees.tsx
+++ src/routes/PageEmployees.tsx
@@ -1,48 +1,84 @@
-import { useContext, useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
-import { Navigate } from 'react-router-dom'
+import _ from 'lodash'
 
-import { EmployeeContext } from '../contexts/EmployeeContext'
+import { Grid, TextField } from '@mui/material'
+
+import { useCookies } from 'react-cookie'
 
 import { Employee } from '../types/employee'
 
-import { getAllEmployees } from '../api/employees/employee'
+import { getEmployees } from '../api/employees/employee'
 
 import Drawer from '../components/Drawer'
 import EmployeesList from '../components/Employees/EmployeesList'
-import ForceLogin from '../components/ForceLogin'
 import LoadingWrapper from '../components/LoadingWrapper'
 
+import { BOHEMIA_PADEL_JWT } from '../types/userCookie'
+import EmployeeCreate from '../components/Employees/EmployeeCreate'
+
 const PageEmployees = () => {
-  const employeeContext = useContext(EmployeeContext)
+  const [cookies] = useCookies([BOHEMIA_PADEL_JWT])
+  const token = cookies[BOHEMIA_PADEL_JWT].token
 
   const [employeeList, setEmployeeList] = useState<Employee[]>([])
+  const [searchValue, setSearchValue] = useState('')
   const [loading, setLoading] = useState(true)
-  if (employeeContext?.employee?.role.id !== 3) {
-    return <Navigate to="/"/>
-  }
+
+  const debouncedSearch = useCallback(
+    _.debounce(async (newValue: string) => {
+      await handleSearchEmployees(newValue)
+    }, 1000),
+    []
+  )
+
   useEffect(() => {
-    const getData = async () => {
-      const result = await getAllEmployees()
-      if (result) {
-        setEmployeeList(result)
-        setLoading(false)
-      } else {
-        setEmployeeList([])
-        setLoading(false)
+    debouncedSearch(searchValue)
+    return debouncedSearch.cancel
+  }, [searchValue, debouncedSearch])
+
+  const handleSearchEmployees = async (newValue: string) => {
+    try {
+      if (newValue !== '') {
+        setLoading(true)
+        const result = await getEmployees({ search: newValue, includeDisabled: 1 }, token)
+        if (result) {
+          setEmployeeList(result)
+        } else {
+          setEmployeeList([])
+        }
       }
+    } catch (error) {
+      setEmployeeList([])
+    } finally {
+      setLoading(false)
     }
-    getData()
-  }, [])
+  }
+
   return (
-    <ForceLogin>
-      <Drawer>
-        <h1>Página trabajadores</h1>
+    <Drawer>
+      <h1>Página trabajadores</h1>
+      <EmployeeCreate/>
+      <Grid container>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            id="searchValue"
+            label="Nombre/Apellido/Correo electrónico de trabajador"
+            variant="filled"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+        </Grid>
+      </Grid>
+      {searchValue === '' ? (
+        <h3>Empiece a escribir para buscar</h3>
+      ) : (
         <LoadingWrapper loading={loading}>
           <EmployeesList employees={employeeList} />
         </LoadingWrapper>
-      </Drawer>
-    </ForceLogin>
+      )}
+    </Drawer>
   )
 }
 
