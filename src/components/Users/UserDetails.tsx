cb05e1c1949d6225e0bd15b19341c1bf06af521d
jokinz <53506251+jokinz@users.noreply.github.com>
2024-08-15 13:13:57 -0500

    Update UserDetails.tsx

--- src/components/Users/UserDetails.tsx
+++ src/components/Users/UserDetails.tsx
@@ -1,9 +1,12 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 
-import { User } from '../../types/user'
+import { useCookies } from 'react-cookie'
 
 import {
   Button,
+  Dialog,
+  DialogActions,
+  DialogTitle,
   FormControl,
   Grid,
   InputLabel,
@@ -12,30 +15,72 @@ import {
   SelectChangeEvent,
   TextField,
 } from '@mui/material'
+
+import { enqueueSnackbar } from 'notistack'
+
+import { User } from '../../types/user'
+
 import { updateUserById } from '../../api/users/user'
+
+import { BOHEMIA_PADEL_JWT } from '../../types/userCookie'
+
+import { areValuesDifferent, getDifferences } from '../../utils'
+
+import DetailsWrapper from '../DetailsWrapper'
+import LoadingWrapper from '../LoadingWrapper'
+
 type props = {
   user: User
+  updateUser: (updatedUser: User) => void
 }
 
-const UserDetails = ({ user }: props) => {
-  const [userData, setUserData] = useState<User>(user)
+const UserDetails = ({ user, updateUser }: props) => {
+  const [cookies] = useCookies([BOHEMIA_PADEL_JWT])
+  const token = cookies[BOHEMIA_PADEL_JWT].token
+
+  const initialUser = useMemo(() => user, [])
+
+  const [showModal, setShowModal] = useState(false)
+  const [updateLoading, setUpdateLoading] = useState(false)
+
+  const handleUpdateButtonClick = () => {
+    setShowModal(true)
+  }
+
+  const handleClose = () => {
+    setShowModal(false)
+  }
+
+  const handleConfirmationClick = async () => {
+    try {
+      setUpdateLoading(true)
+      const result = await updateUserById(
+        user.id,
+        getDifferences(initialUser, user),
+        token
+      )
+      if (result) {
+        setShowModal(false)
+        enqueueSnackbar('Usuario actualizado', { variant: 'success' })
+      }
+    } catch (error) {
+      enqueueSnackbar('Error actualizando', { variant: 'error' })
+    } finally {
+      setShowModal(false)
+      setUpdateLoading(false)
+    }
+  }
+
   const handleDocumentTypeChange = (event: SelectChangeEvent) => {
-    setUserData((prevUserData) => {
-      return { ...prevUserData, documentType: event.target.value }
-    })
+    updateUser({ ...user, documentType: event.target.value })
   }
   const handleDistrictChange = (event: SelectChangeEvent) => {
-    setUserData((prevUserData) => {
-      return { ...prevUserData, district: event.target.value }
-    })
+    updateUser({ ...user, district: event.target.value })
   }
-  const handleUpdateUserClick = async () => {
-    const response = await updateUserById(userData)
-    console.log(response)
-  }
+
   return (
-    <Grid container width={'50%'} rowSpacing={3} gridColumn={2}>
-      <Grid item xs={12}>
+    <DetailsWrapper>
+      <Grid textAlign={'center'} item xs={12}>
         <h1>Actualizar usuario</h1>
       </Grid>
       <Grid item xs={6}>
@@ -43,12 +88,10 @@ const UserDetails = ({ user }: props) => {
           id="firstName"
           label="Nombre"
           variant="filled"
-          value={userData.firstName}
-          onChange={(e) =>
-            setUserData((prev) => {
-              return { ...prev, firstName: e.target.value }
-            })
-          }
+          value={user.firstName}
+          onChange={(e) => {
+            updateUser({ ...user, firstName: e.target.value })
+          }}
         />
       </Grid>
       <Grid item xs={6}>
@@ -56,12 +99,10 @@ const UserDetails = ({ user }: props) => {
           id="lastName"
           label="Apellido"
           variant="filled"
-          value={userData.lastName}
-          onChange={(e) =>
-            setUserData((prev) => {
-              return { ...prev, lastName: e.target.value }
-            })
-          }
+          value={user.lastName}
+          onChange={(e) => {
+            updateUser({ ...user, lastName: e.target.value })
+          }}
         />
       </Grid>
       <Grid item xs={12}>
@@ -70,12 +111,10 @@ const UserDetails = ({ user }: props) => {
           id="email"
           label="Correo electrónico"
           variant="filled"
-          value={userData.email}
-          onChange={(e) =>
-            setUserData((prev) => {
-              return { ...prev, email: e.target.value }
-            })
-          }
+          value={user.email}
+          onChange={(e) => {
+            updateUser({ ...user, email: e.target.value })
+          }}
           fullWidth
         />
       </Grid>
@@ -87,11 +126,11 @@ const UserDetails = ({ user }: props) => {
           <Select
             labelId="demo-simple-select-label"
             id="demo-simple-select"
-            value={userData.documentType}
+            value={user.documentType}
             label="Tipo"
             onChange={handleDocumentTypeChange}
           >
-            <MenuItem value={'dni'}>DNI</MenuItem>
+            <MenuItem value={'DNI'}>DNI</MenuItem>
             <MenuItem value={'other'}>Otro</MenuItem>
           </Select>
         </FormControl>
@@ -101,12 +140,10 @@ const UserDetails = ({ user }: props) => {
           id="documentNumber"
           label="N° de documento"
           variant="filled"
-          value={userData.documentNumber}
-          onChange={(e) =>
-            setUserData((prev) => {
-              return { ...prev, documentNumber: e.target.value }
-            })
-          }
+          value={user.documentNumber}
+          onChange={(e) => {
+            updateUser({ ...user, documentNumber: e.target.value })
+          }}
         />
       </Grid>
       <Grid item xs={6}>
@@ -115,13 +152,63 @@ const UserDetails = ({ user }: props) => {
           <Select
             labelId="demo-simple-select-label"
             id="demo-simple-select"
-            value={userData.district}
+            value={user.district}
             label="Distrito"
             onChange={handleDistrictChange}
           >
-            <MenuItem value={'d1'}>d1</MenuItem>
-            <MenuItem value={'d2'}>d2</MenuItem>
-            <MenuItem value={'d3'}>d3</MenuItem>
+            <MenuItem value={'Ancón'}>Ancón</MenuItem>
+            <MenuItem value={'Ate'}>Ate</MenuItem>
+            <MenuItem value={'Barranco'}>Barranco</MenuItem>
+            <MenuItem value={'Breña'}>Breña</MenuItem>
+            <MenuItem value={'Carabayllo'}>Carabayllo</MenuItem>
+            <MenuItem value={'Chaclacayo'}>Chaclacayo</MenuItem>
+            <MenuItem value={'Chorrillos'}>Chorrillos</MenuItem>
+            <MenuItem value={'Cieneguilla'}>Cieneguilla</MenuItem>
+            <MenuItem value={'Comas'}>Comas</MenuItem>
+            <MenuItem value={'El Agustino'}>El Agustino</MenuItem>
+            <MenuItem value={'Independencia'}>Independencia</MenuItem>
+            <MenuItem value={'Jesús María'}>Jesús María</MenuItem>
+            <MenuItem value={'La Molina'}>La Molina</MenuItem>
+            <MenuItem value={'La Victoria'}>La Victoria</MenuItem>
+            <MenuItem value={'Lima'}>Lima</MenuItem>
+            <MenuItem value={'Lince'}>Lince</MenuItem>
+            <MenuItem value={'Los Olivos'}>Los Olivos</MenuItem>
+            <MenuItem value={'Lurigancho'}>Lurigancho</MenuItem>
+            <MenuItem value={'Lurín'}>Lurín</MenuItem>
+            <MenuItem value={'Magdalena del Mar'}>Magdalena del Mar</MenuItem>
+            <MenuItem value={'Miraflores'}>Miraflores</MenuItem>
+            <MenuItem value={'Pachacámac'}>Pachacámac</MenuItem>
+            <MenuItem value={'Pucusana'}>Pucusana</MenuItem>
+            <MenuItem value={'Pueblo Libre'}>Pueblo Libre</MenuItem>
+            <MenuItem value={'Puente Piedra'}>Puente Piedra</MenuItem>
+            <MenuItem value={'Punta Hermosa'}>Punta Hermosa</MenuItem>
+            <MenuItem value={'Punta Negra'}>Punta Negra</MenuItem>
+            <MenuItem value={'Rímac'}>Rímac</MenuItem>
+            <MenuItem value={'San Bartolo'}>San Bartolo</MenuItem>
+            <MenuItem value={'San Borja'}>San Borja</MenuItem>
+            <MenuItem value={'San Isidro'}>San Isidro</MenuItem>
+            <MenuItem value={'San Juan de Lurigancho'}>
+              San Juan de Lurigancho
+            </MenuItem>
+            <MenuItem value={'San Juan de Miraflores'}>
+              San Juan de Miraflores
+            </MenuItem>
+            <MenuItem value={'San Luis'}>San Luis</MenuItem>
+            <MenuItem value={'San Martín de Porres'}>
+              San Martín de Porres
+            </MenuItem>
+            <MenuItem value={'San Miguel'}>San Miguel</MenuItem>
+            <MenuItem value={'Santa Anita'}>Santa Anita</MenuItem>
+            <MenuItem value={'Santa María del Mar'}>
+              Santa María del Mar
+            </MenuItem>
+            <MenuItem value={'Santa Rosa'}>Santa Rosa</MenuItem>
+            <MenuItem value={'Santiago de Surco'}>Santiago de Surco</MenuItem>
+            <MenuItem value={'Surquillo'}>Surquillo</MenuItem>
+            <MenuItem value={'Villa El Salvador'}>Villa El Salvador</MenuItem>
+            <MenuItem value={'Villa María del Triunfo'}>
+              Villa María del Triunfo
+            </MenuItem>
           </Select>
         </FormControl>
       </Grid>
@@ -131,20 +218,43 @@ const UserDetails = ({ user }: props) => {
           id="tel"
           label="Teléfono"
           variant="filled"
-          value={userData.phone}
-          onChange={(e) =>
-            setUserData((prev) => {
-              return { ...prev, phone: e.target.value }
-            })
-          }
+          value={user.phone}
+          onChange={(e) => {
+            updateUser({ ...user, phone: e.target.value })
+          }}
         />
       </Grid>
       <Grid item xs={12}>
-        <Button variant="contained" onClick={handleUpdateUserClick}>
+        <Button
+          disabled={!areValuesDifferent(user, initialUser)}
+          variant="contained"
+          onClick={handleUpdateButtonClick}
+        >
           Actualizar usuario
         </Button>
       </Grid>
-    </Grid>
+      <Dialog
+        open={showModal}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {'¿Actualizar el estado del usuario?'}
+        </DialogTitle>
+        <DialogActions>
+          <Button
+            disabled={updateLoading}
+            onClick={handleConfirmationClick}
+            autoFocus
+          >
+            <LoadingWrapper loading={updateLoading}> </LoadingWrapper>
+            Sí, actualizar
+          </Button>
+          <Button onClick={handleClose}>Cancelar</Button>
+        </DialogActions>
+      </Dialog>
+    </DetailsWrapper>
   )
 }
 
