9df077aac5b973524f97f4f138009fca1eaa2e51
jokinz <53506251+jokinz@users.noreply.github.com>
2024-08-17 16:48:29 -0500

    added requiredRole logic

--- src/components/ForceLogin.tsx
+++ src/components/ForceLogin.tsx
@@ -4,14 +4,29 @@ import { EmployeeContext } from '../contexts/EmployeeContext'
 
 import PageLogin from '../routes/PageLogin'
 import PageDisabled from '../routes/PageDisabled'
+import PageDashboard from '../routes/PageDashboard'
 
-const ForceLogin = ({ children }: { children: React.ReactNode }) => {
+const ForceLogin = ({
+  children,
+  requiredRole,
+}: {
+  children: React.ReactNode
+  requiredRole?: (2 | 3)[]
+}) => {
   const employeeContext = useContext(EmployeeContext)
-  if (!employeeContext?.employee) {
-    return <PageLogin />
-  }
-  if (employeeContext.employee.enabled === false) {
-    return <PageDisabled />
+  if (employeeContext) {
+    if (!employeeContext.employee) {
+      return <PageLogin />
+    } else {
+      const userRoleId = employeeContext.employee.role.id
+      if (requiredRole && !requiredRole.find((id) => id === userRoleId)) {
+        return <PageDashboard />
+      }
+    }
+
+    if (employeeContext.employee.enabled === false) {
+      return <PageDisabled />
+    }
   }
   return children
 }
