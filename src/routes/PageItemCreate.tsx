a4155f019348061bdcfe5e3bdbba727a1a81ab1d
jokinz <53506251+jokinz@users.noreply.github.com>
2024-08-23 00:01:54 -0500

    Create PageItemCreate.tsx

---
+++ src/routes/PageItemCreate.tsx
@@ -0,0 +1,12 @@
+import Drawer from '../components/Drawer'
+import ItemCreate from '../components/Items/ItemCreate'
+
+const PageItemCreate = () => {
+  return (
+    <Drawer>
+      <ItemCreate />
+    </Drawer>
+  )
+}
+
+export default PageItemCreate
