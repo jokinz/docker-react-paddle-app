c4c78a3ccb46e42f9e2bfd2f2626abcb2e517998
jokinz <53506251+jokinz@users.noreply.github.com>
2024-08-23 22:55:10 -0500

    added createItemCategory methods

--- src/api/items/itemCategory.ts
+++ src/api/items/itemCategory.ts
@@ -4,12 +4,15 @@ import { AxiosResponse } from 'axios'
 
 import { ItemCategory } from '../../types/item'
 import { GetItemCategoriesResponse } from '../../types/responses/GetItemCategoriesResponse'
+import { CreateItemCategorySchema } from '../../types/schemas/CreateItemCategorySchema'
+import { CreateItemCategoryResponse } from '../../types/responses/CreateItemCategoryResponse'
+
 export const getAllItemCategories = async (
   token: string
 ): Promise<ItemCategory[] | undefined> => {
   try {
     const axiosResponse: AxiosResponse<GetItemCategoriesResponse> =
-      await axios.get('/item-categories/value', {
+      await axios.get('/item-categories', {
         headers: {
           Authorization: `Bearer ${token}`,
         },
@@ -18,5 +21,49 @@ export const getAllItemCategories = async (
       return axiosResponse.data.data.rows
     }
     throw new Error('Error descargando las categorías')
+  } catch (error) {}
+}
+
+export const createItemCategoryWithResponse = async (
+  itemCategory: CreateItemCategorySchema,
+  token: string
+): Promise<ItemCategory | undefined> => {
+  try {
+    const axiosResponse: AxiosResponse<CreateItemCategoryResponse> =
+      await axios.post(
+        `/item-categories`,
+        { ...itemCategory, returning: true },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+    if (axiosResponse.status === 200 && axiosResponse.data.statusCode === 200) {
+      return axiosResponse.data.data
+    }
+    throw new Error('Error creando la categoría')
+  } catch (error) {}
+}
+
+export const createItemCategoryNoResponse = async (
+  itemCategory: CreateItemCategorySchema,
+  token: string
+): Promise<true | undefined> => {
+  try {
+    const axiosResponse: AxiosResponse<CreateItemCategoryResponse> =
+      await axios.post(
+        `/item-categories`,
+        { ...itemCategory, returning: false },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+    if (axiosResponse.status === 204) {
+      return true
+    }
+    throw new Error('Error creando la categoría')
   } catch (error) {}
 }
