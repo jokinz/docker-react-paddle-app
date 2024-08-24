d0ed93d79ee94b1f99c7e929170f3105662b66a8
jokinz <53506251+jokinz@users.noreply.github.com>
2024-08-24 18:58:44 -0500

    Create employeeRole.ts

---
+++ src/api/employees/employeeRole.ts
@@ -0,0 +1,25 @@
+import axios from '../axios'
+
+import { AxiosResponse } from 'axios'
+
+import { EmployeeRoleResponse } from '../../types/employee'
+import { GetEmployeeRolesResponse } from '../../types/responses/GetEmployeeRolesResponse'
+
+export const getAllEmployeeRoles = async (
+  token: string
+): Promise<EmployeeRoleResponse[] | undefined> => {
+  try {
+    const axiosResponse: AxiosResponse<GetEmployeeRolesResponse> =
+      await axios.get('/roles/value', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+    if (axiosResponse.status === 200 && axiosResponse.data.statusCode === 200) {
+      return axiosResponse.data.data
+    }
+    throw new Error('Error descargando los roles')
+  } catch (error) {
+    throw Error
+  }
+}
