e9a86a01d1fd80293e5d1811c3d1c8f745d5db31
jokinz <53506251+jokinz@users.noreply.github.com>
2024-08-24 21:07:02 -0500

    added getPlayingFieldById

--- src/api/playingField.ts
+++ src/api/playingField.ts
@@ -5,6 +5,7 @@ import { AxiosResponse } from 'axios'
 import { PlayingField } from '../types/playingField'
 import { GetPlayingFieldsResponse } from '../types/responses/GetPlayingFieldsResponse'
 import { GetPlayingFieldsSchema } from '../types/schemas/GetPlayingFieldsSchema'
+import { GetPlayingFieldByIdResponse } from '../types/responses/GetPlayingFieldByIdResponse'
 
 export const getPlayingFields = async (
   playingFieldsSchema: GetPlayingFieldsSchema,
@@ -26,3 +27,23 @@ export const getPlayingFields = async (
     throw error
   }
 }
+
+export const getPlayingFieldById = async (
+  playingFieldId: number,
+  token: string
+): Promise<PlayingField | undefined> => {
+  try {
+    const axiosResponse: AxiosResponse<GetPlayingFieldByIdResponse> =
+      await axios.get(`/playing-fields/${playingFieldId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+    if (axiosResponse.status === 200 && axiosResponse.data.statusCode === 200) {
+      return axiosResponse.data.data
+    }
+    throw new Error('Error descargando datos del campo de juego')
+  } catch (error) {}
+  return
+}
+
