58e34476838c6c429c3dbddfdce371cf8d01c282
jokinz <53506251+jokinz@users.noreply.github.com>
2024-08-24 19:50:41 -0500

    Create establishments.ts

---
+++ src/api/establishments.ts
@@ -0,0 +1,25 @@
+import axios from './axios'
+
+import { AxiosResponse } from 'axios'
+
+import { Establishment } from '../types/establishment'
+import { GetEstablishmentsResponse } from '../types/responses/GetEstablishmentsResponse'
+
+export const getAllItemCategories = async (
+  token: string
+): Promise<Pick<Establishment, 'id' | 'name'>[] | undefined> => {
+  try {
+    const axiosResponse: AxiosResponse<GetEstablishmentsResponse> =
+      await axios.get('/establishments/value', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+    if (axiosResponse.status === 200 && axiosResponse.data.statusCode === 200) {
+      return axiosResponse.data.data
+    }
+    throw new Error('Error descargando los establecimientos')
+  } catch (error) {
+    throw error
+  }
+}
