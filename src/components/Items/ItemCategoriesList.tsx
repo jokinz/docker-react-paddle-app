8ec8198b66d28bea3084d1b2d68114637e3518ec
jokinz <53506251+jokinz@users.noreply.github.com>
2024-08-27 18:15:08 -0500

    Create ItemCategoriesList.tsx

---
+++ src/components/Items/ItemCategoriesList.tsx
@@ -0,0 +1,49 @@
+import { ItemCategory } from '../../types/item'
+
+import { DataGrid, DataGridProps, GridColDef } from '@mui/x-data-grid'
+import { getDataGridProps } from '../../utils'
+import Link from '../ResultsStyledLink'
+import { Avatar } from '@mui/material'
+
+type props = { categories: ItemCategory[] }
+const ItemCategoriesList = ({ categories }: props) => {
+  const columns: GridColDef<(typeof rows)[number]>[] = [
+    {
+      field: 'icon',
+      headerName: '',
+      renderCell: (params) => (
+        <Avatar src={params.row.icon ? params.row.icon : ''} />
+      ),
+      display: 'flex',
+      align: 'center'
+    },
+    {
+      field: 'name',
+      headerName: 'Nombre',
+      flex: 1,
+      renderCell: (params) => (
+        <Link to={`/categories/${params.row.id}`}>{params.value}</Link>
+      ),
+    },
+  ]
+  const rows = categories
+  const dataGridProps: DataGridProps = getDataGridProps(rows, columns)
+
+  if (categories.length > 0) {
+    return (
+      <div>
+        <h1>Resultado</h1>
+        <DataGrid {...dataGridProps} />
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      Lista de categorías <br />
+      Ninguna categoría para mostrar
+    </div>
+  )
+}
+
+export default ItemCategoriesList
