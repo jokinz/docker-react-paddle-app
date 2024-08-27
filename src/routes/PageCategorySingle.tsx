1dd2ac4695447d18507806fbc0e6fb9dc3bc6088
jokinz <53506251+jokinz@users.noreply.github.com>
2024-08-27 18:21:13 -0500

    Create PageCategorySingle.tsx

---
+++ src/routes/PageCategorySingle.tsx
@@ -0,0 +1,63 @@
+import { useContext, useEffect, useState } from 'react'
+
+import { useParams } from 'react-router-dom'
+
+import { ItemCategory } from '../types/item'
+
+import { getItemCategoryById } from '../api/items/itemCategory'
+
+import Drawer from '../components/Drawer'
+import ItemCategoryDetails from '../components/Items/ItemCategoryDetails'
+import { EmployeeContext } from '../contexts/EmployeeContext'
+import LoadingWrapper from '../components/LoadingWrapper'
+
+const PageCategorySingle = () => {
+  const params = useParams<{ categoryId: string }>()
+
+  const employeeContext = useContext(EmployeeContext)
+  const token = employeeContext?.token
+
+  const [loading, setLoading] = useState(true)
+  const [itemCategory, setItemCategory] = useState<ItemCategory | null>(null)
+
+  const updateItemCategory = (updatedItemCategory: ItemCategory) => {
+    setItemCategory(updatedItemCategory)
+  }
+
+  useEffect(() => {
+    const getItemCategoryData = async () => {
+      try {
+        if (params.categoryId && token && token !== '') {
+          const itemCategoryResponse = await getItemCategoryById(
+            parseInt(params.categoryId),
+            token
+          )
+          if (itemCategoryResponse) {
+            setItemCategory(itemCategoryResponse)
+          }
+        }
+      } catch (error) {
+      } finally {
+        setLoading(false)
+      }
+    }
+    getItemCategoryData()
+  }, [])
+
+  return (
+    <Drawer>
+      <LoadingWrapper loading={loading}>
+        {itemCategory !== null ? (
+          <ItemCategoryDetails
+            itemCategory={itemCategory}
+            updateItemCategory={updateItemCategory}
+          />
+        ) : (
+          <h1>Categoría no encontrada</h1>
+        )}
+      </LoadingWrapper>
+    </Drawer>
+  )
+}
+
+export default PageCategorySingle
