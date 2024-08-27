eb590bb07511a423954f026a4bb1e926054681d9
jokinz <53506251+jokinz@users.noreply.github.com>
2024-08-26 19:28:21 -0500

    updated ui to DataGrid

--- src/components/Reservations/ReservationsList.tsx
+++ src/components/Reservations/ReservationsList.tsx
@@ -1,26 +1,53 @@
 import { Reservation as ReservationType } from '../../types/reservation'
 
-import { Grid, List } from '@mui/material'
-
-import ReservationItem from './ReservationItem'
+import { DataGrid, DataGridProps, GridColDef } from '@mui/x-data-grid'
+import { getDataGridProps } from '../../utils'
+import Link from '../ResultsStyledLink'
 
 type props = { reservations: ReservationType[] }
+
 const ReservationsList = ({ reservations }: props) => {
+  const columns: GridColDef<(typeof rows)[number]>[] = [
+    {
+      field: 'code',
+      headerName: 'Código',
+      flex: 1,
+      renderCell: (params) => (
+        <Link to={`/reservations/${params.row.id}`}>{params.value}</Link>
+      ),
+    },
+    {
+      field: 'date',
+      headerName: 'Fecha',
+      flex: 1,
+    },
+    {
+      field: 'time',
+      headerName: 'Horario',
+      flex: 1,
+    },
+    {
+      field: 'payed',
+      headerName: 'Pagado',
+      flex: 1,
+    },
+  ]
+  const rows = reservations.map((reservation) => {
+    return {
+      id: reservation.id,
+      code: reservation.code,
+      date: reservation.schedule.date,
+      time: `${reservation.schedule.startTime} - ${reservation.schedule.endTime}`,
+      payed: reservation.payed ? 'Sí' : 'No',
+    }
+  })
+  const dataGridProps: DataGridProps = getDataGridProps(rows, columns)
+
   if (reservations.length > 0) {
     return (
       <div>
         <h1>Resultado</h1>
-        <Grid item xs={12} md={6}>
-          <List dense={false}>
-            {reservations.map((reservation, index) => (
-              <ReservationItem
-                key={index}
-                index={index}
-                reservation={reservation}
-              />
-            ))}
-          </List>
-        </Grid>
+        <DataGrid {...dataGridProps} />
       </div>
     )
   }
