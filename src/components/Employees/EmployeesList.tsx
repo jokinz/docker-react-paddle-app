a5f7c4c57d2cb18cde93e8cc32fafe423f2d86fd
jokinz <53506251+jokinz@users.noreply.github.com>
2024-08-27 02:10:01 -0500

    updated ui to DataGrid

--- src/components/Employees/EmployeesList.tsx
+++ src/components/Employees/EmployeesList.tsx
@@ -1,22 +1,52 @@
-import { Grid, List } from '@mui/material'
-
 import { Employee as EmployeeType } from '../../types/employee'
 
-import EmployeeItem from './EmployeeItem'
+import { DataGrid, DataGridProps, GridColDef } from '@mui/x-data-grid'
+import { getDataGridProps } from '../../utils'
+import Link from '../ResultsStyledLink'
 
 type props = { employees: EmployeeType[] }
 const EmployeesList = ({ employees }: props) => {
+  const columns: GridColDef<(typeof rows)[number]>[] = [
+    {
+      field: 'fullName',
+      headerName: 'Nombre',
+      flex: 1,
+      renderCell: (params) => (
+        <Link to={`/employees/${params.row.id}`}>{params.value}</Link>
+      ),
+    },
+    {
+      field: 'email',
+      headerName: 'Email',
+      flex: 1,
+    },
+    {
+      field: 'enabled',
+      headerName: 'Habilitado',
+      flex: 1,
+    },
+    {
+      field: 'role',
+      headerName: 'Rol',
+      flex: 1,
+    },
+  ]
+  const rows = employees.map((employee) => {
+    return {
+      id: employee.id,
+      fullName: `${employee.firstName} - ${employee.lastName}`,
+      email: employee.email,
+      enabled: employee.enabled ? 'Sí' : 'No',
+      role: employee.role.roleName,
+    }
+  })
+  const dataGridProps: DataGridProps = getDataGridProps(rows, columns)
+
   if (employees.length > 0) {
     return (
       <div>
         <h1>Resultado</h1>
-        <Grid item xs={12} md={6}>
-          <List dense={false}>
-            {employees.map((employee, index) => (
-              <EmployeeItem key={index} employee={employee} />
-            ))}
-          </List>
-        </Grid>
+        <DataGrid {...dataGridProps} />
       </div>
     )
   }
