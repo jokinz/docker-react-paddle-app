e9e44caf0fc1dbf5f35e86650fe6f79fd9b91fcd
jokinz <53506251+jokinz@users.noreply.github.com>
2024-08-26 19:28:56 -0500

    updated ui to DataGrid

--- src/components/Users/UsersList.tsx
+++ src/components/Users/UsersList.tsx
@@ -1,22 +1,57 @@
-import { Grid, List } from '@mui/material'
+import { Avatar } from '@mui/material'
 
 import { User as UserType } from '../../types/user'
 
-import UserItem from './UserItem'
+import { DataGrid, DataGridProps, GridColDef } from '@mui/x-data-grid'
+import { getDataGridProps } from '../../utils'
+import Link from '../ResultsStyledLink'
 
 type props = { users: UserType[] }
 const UsersList = ({ users }: props) => {
+  const columns: GridColDef<(typeof rows)[number]>[] = [
+    {
+      field: 'avatar',
+      headerName: '',
+      renderCell: (params) => (
+        <Avatar src={params.row.avatar ? params.row.avatar : ''} />
+      ),
+      align: 'center',
+    },
+    {
+      field: 'fullName',
+      headerName: 'Nombre',
+      flex: 1,
+      renderCell: (params) => (
+        <Link to={`/users/${params.row.id}`}>{params.value}</Link>
+      ),
+    },
+    {
+      field: 'email',
+      headerName: 'Email',
+      flex: 1,
+    },
+    {
+      field: 'phone',
+      headerName: 'Teléfono',
+      flex: 1,
+    },
+  ]
+  const rows = users.map((user) => {
+    return {
+      id: user.id,
+      fullName: `${user.firstName} - ${user.lastName}`,
+      email: user.email,
+      phone: user.phone,
+      avatar: user.profileImage,
+    }
+  })
+  const dataGridProps: DataGridProps = getDataGridProps(rows, columns)
+
   if (users.length > 0) {
     return (
       <div>
         <h1>Resultado</h1>
-        <Grid item xs={12} md={6}>
-          <List dense={false}>
-            {users.map((user, index) => (
-              <UserItem key={index} user={user} />
-            ))}
-          </List>
-        </Grid>
+        <DataGrid {...dataGridProps} />
       </div>
     )
   }
