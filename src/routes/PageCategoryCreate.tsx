d194ba8592db22914a072f335ce68ad2ab9046d8
jokinz <53506251+jokinz@users.noreply.github.com>
2024-08-27 18:21:10 -0500

    Create PageCategoryCreate.tsx

---
+++ src/routes/PageCategoryCreate.tsx
@@ -0,0 +1,12 @@
+import Drawer from '../components/Drawer'
+import ItemCategoryCreate from '../components/Items/ItemCategoryCreate'
+
+const PageCategoryCreate = () => {
+  return (
+    <Drawer>
+      <ItemCategoryCreate />
+    </Drawer>
+  )
+}
+
+export default PageCategoryCreate
