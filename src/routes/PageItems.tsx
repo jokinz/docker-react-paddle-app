26c1a76953ddaa12ec95fa718248dac76240d160
jokinz <53506251+jokinz@users.noreply.github.com>
2024-08-27 02:11:26 -0500

    updated ui and added search filters

--- src/routes/PageItems.tsx
+++ src/routes/PageItems.tsx
@@ -1,4 +1,8 @@
-import { useContext, useEffect, useState } from 'react'
+import { useCallback, useContext, useEffect, useState } from 'react'
+
+import _ from 'lodash'
+
+import { FormControlLabel, Grid, Switch, TextField } from '@mui/material'
 
 import { enqueueSnackbar } from 'notistack'
 
@@ -6,29 +10,50 @@ import { EmployeeContext } from '../contexts/EmployeeContext'
 
 import { Item } from '../types/item'
 
-import { getAllItems } from '../api/items/item'
+import { getItems } from '../api/items/item'
 
 import Drawer from '../components/Drawer'
 import ItemsList from '../components/Items/ItemsList'
 import LoadingWrapper from '../components/LoadingWrapper'
-import SkeletonList from '../components/SkeletonList'
 
 const PageItems = () => {
   const employeeContext = useContext(EmployeeContext)
   const token = employeeContext?.token
 
-  const [itemList, setItemList] = useState<Item[]>([])
+  const [itemsList, setItemsList] = useState<Item[]>([])
+  const [searchValue, setSearchValue] = useState('')
+  const [includeDisabled, setIncludeDisabled] = useState<boolean>(true)
   const [loading, setLoading] = useState(true)
+
+  const debouncedSearch = useCallback(
+    _.debounce(async (newValue: string) => {
+      await handleSearchItems(newValue)
+    }, 1000),
+    []
+  )
+
+  useEffect(() => {
+    debouncedSearch(searchValue)
+    return debouncedSearch.cancel
+  }, [searchValue, debouncedSearch])
 
   useEffect(() => {
     const getData = async () => {
       if (token && token !== '') {
         try {
-          const result = await getAllItems({ search: '' }, token)
+          setLoading(true)
+          const result = await getItems(
+            {
+              search: searchValue,
+              includeDisabled: includeDisabled ? 1 : 0,
+              records: 5,
+            },
+            token
+          )
           if (result) {
-            setItemList(result)
+            setItemsList(result)
           } else {
-            setItemList([])
+            setItemsList([])
           }
         } catch (error) {
           enqueueSnackbar('Error descargando items', { variant: 'error' })
@@ -40,12 +65,69 @@ const PageItems = () => {
       }
     }
     getData()
-  }, [])
+  }, [includeDisabled])
+
+  const handleSearchItems = async (newValue: string) => {
+    try {
+      if (newValue !== '' && token && token !== '') {
+        setLoading(true)
+        const result = await getItems(
+          {
+            search: newValue,
+            includeDisabled: includeDisabled ? 1 : 0,
+            records: 5,
+          },
+          token
+        )
+        if (result) {
+          setItemsList(result)
+        } else {
+          setItemsList([])
+        }
+      }
+    } catch (error) {
+      enqueueSnackbar(`Error cargando ítems`, { variant: 'error' })
+      setItemsList([])
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const handleIncludeDisabledClick = () => {
+    setIncludeDisabled((prev) => !prev)
+  }
 
   return (
     <Drawer>
-      <LoadingWrapper loading={loading} skeleton={<SkeletonList />}>
-        <ItemsList items={itemList} />
+      <h1>Página Ítems</h1>
+      <Grid container>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            id="searchValue"
+            label="Nombre del ítem"
+            variant="filled"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <FormControlLabel
+            control={
+              <Switch
+                aria-label="Activado"
+                checked={includeDisabled}
+                onClick={handleIncludeDisabledClick}
+              />
+            }
+            label="Buscar desactivados"
+            labelPlacement="start"
+          />
+        </Grid>
+      </Grid>
+      {/* {searchValue === '' && <h3>Empiece a escribir para buscar</h3>} */}
+      <LoadingWrapper loading={loading}>
+        <ItemsList items={itemsList} />
       </LoadingWrapper>
     </Drawer>
   )
