f3ac735c9b8a8460a64b07a6cb48770ee7faece9
jokinz <53506251+jokinz@users.noreply.github.com>
2024-08-27 22:47:13 -0500

    Create PagePlayingFieldCreate.tsx

---
+++ src/routes/PagePlayingFieldCreate.tsx
@@ -0,0 +1,12 @@
+import Drawer from '../components/Drawer'
+import PlayingFieldCreate from '../components/PlayingFields/PlayingFieldCreate'
+
+const PagePlayingFieldCreate = () => {
+  return (
+    <Drawer>
+      <PlayingFieldCreate />
+    </Drawer>
+  )
+}
+
+export default PagePlayingFieldCreate
