bcfe26946fc7e85959e8880227ada5fb5cd6223f
jokinz <53506251+jokinz@users.noreply.github.com>
2024-08-27 21:19:49 -0500

    Create PagePlayingFieldSingle.tsx

---
+++ src/routes/PagePlayingFieldSingle.tsx
@@ -0,0 +1,63 @@
+import { useContext, useEffect, useState } from 'react'
+
+import { useParams } from 'react-router-dom'
+
+import { PlayingField } from '../types/playingField'
+
+import { getPlayingFieldById } from '../api/playingField'
+
+import Drawer from '../components/Drawer'
+import PlayingFieldDetails from '../components/PlayingFields/PlayingFieldDetails'
+import LoadingWrapper from '../components/LoadingWrapper'
+import { EmployeeContext } from '../contexts/EmployeeContext'
+
+const PagePlayingFieldSingle = () => {
+  const params = useParams<{ playingFieldId: string }>()
+
+  const employeeContext = useContext(EmployeeContext)
+  const token = employeeContext?.token
+
+  const [loading, setLoading] = useState(true)
+  const [playingField, setPlayingField] = useState<PlayingField | null>(null)
+
+  const updatePlayingField = (updatedPlayingField: PlayingField) => {
+    setPlayingField(updatedPlayingField)
+  }
+
+  useEffect(() => {
+    const getPlayingFieldData = async () => {
+      try {
+        if (params.playingFieldId && token && token !== '') {
+          const playingFieldResponse = await getPlayingFieldById(
+            parseInt(params.playingFieldId),
+            token
+          )
+          if (playingFieldResponse) {
+            setPlayingField(playingFieldResponse)
+          }
+        }
+      } catch (error) {
+      } finally {
+        setLoading(false)
+      }
+    }
+    getPlayingFieldData()
+  }, [])
+
+  return (
+    <Drawer>
+      <LoadingWrapper loading={loading}>
+        {playingField !== null ? (
+          <PlayingFieldDetails
+            playingField={playingField}
+            updatePlayingField={updatePlayingField}
+          />
+        ) : (
+          <h1>Campo de juego no encontrado</h1>
+        )}
+      </LoadingWrapper>
+    </Drawer>
+  )
+}
+
+export default PagePlayingFieldSingle
