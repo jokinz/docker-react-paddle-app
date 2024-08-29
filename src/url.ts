a76a14f2776fb634934745f6b71a0d5b17e8a3dd
jokinz <53506251+jokinz@users.noreply.github.com>
2024-08-29 01:01:15 -0500

    Create url.ts

---
+++ src/url.ts
@@ -0,0 +1,23 @@
+export const url = {
+  api: {
+    auth: 'auth',
+    establishments: 'establishments',
+    playingFields: 'playing-fields',
+    reservations: 'reservations',
+    employees: 'employees',
+    roles: 'roles',
+    items: 'items',
+    itemCategories: 'item-categories',
+    users: 'users',
+  },
+  web: {
+    login: 'login',
+    reservations: 'reservations',
+    users: 'users',
+    employees: 'employees',
+    items: 'items',
+    categories: 'categories',
+    playingFields: 'playing-fields',
+    establishments: 'establishments',
+  },
+}
