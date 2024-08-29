7773650177b53a17cdf532214499d54700467d2d
jokinz <53506251+jokinz@users.noreply.github.com>
2024-08-28 21:00:00 -0500

    added optional showAvatar prop

--- src/components/SkeletonTable.tsx
+++ src/components/SkeletonTable.tsx
@@ -1,23 +1,42 @@
 import Skeleton from '@mui/material/Skeleton'
 import { DataGrid, DataGridProps, GridColDef } from '@mui/x-data-grid'
 import { getDataGridProps } from '../utils'
-import { Typography } from '@mui/material'
+import { Avatar, Typography } from '@mui/material'
 
 type SkeletonTableProps = {
   numColumns?: number
   numRows?: number
+  showAvatar?: boolean
 }
 
-const SkeletonTable = ({ numColumns = 5, numRows = 5 }: SkeletonTableProps) => {
-  const columns: GridColDef[] = Array.from(
-    { length: numColumns },
-    (_, index) => ({
+const SkeletonTable = ({
+  numColumns = 5,
+  numRows = 5,
+  showAvatar = false,
+}: SkeletonTableProps) => {
+  const columns: GridColDef[] = [
+    ...(showAvatar
+      ? [
+          {
+            field: 'avatar',
+            headerName: '',
+            width: 80,
+            sortable: false,
+            renderCell: () => (
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <Skeleton width={40} height={40} variant="circular" />
+              </div>
+            ),
+          },
+        ]
+      : []),
+    ...Array.from({ length: numColumns }, (_, index) => ({
       field: `field${index}`,
       flex: 1,
       renderHeader: () => <Skeleton width={100} variant="text" />,
       renderCell: () => <Skeleton width={100} variant="text" />,
-    })
-  )
+    })),
+  ]
 
   const rows = Array.from({ length: numRows }, (_, index) => ({ id: index }))
 
