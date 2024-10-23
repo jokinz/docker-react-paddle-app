65197de19b3266d31209fb27e69210416be1135b
jokinz <53506251+jokinz@users.noreply.github.com>
2024-10-23 00:00:28 -0500

    removed unnecessary Typography

--- src/components/GridTitle.tsx
+++ src/components/GridTitle.tsx
@@ -1,11 +1,9 @@
-import { Grid, Typography } from '@mui/material'
+import { Grid } from '@mui/material'
 
 const GridTitle = ({ children }: { children: React.ReactNode }) => {
   return (
     <Grid textAlign={'center'} item xs={12}>
-      <Typography>
-        <h1>{children}</h1>
-      </Typography>
+      <h1>{children}</h1>
     </Grid>
   )
 }
